aa1e1f33b0511474a2c76320b3d997a9e43d136d
2012056462 <dbxodnr789@gmail.com>
2018-03-30 02:40:24 +0900

    2018/03/30

---
+++ [12100] 2048 (Easy)/[12100] 2048 (Easy)/main.cpp
@@ -0,0 +1,188 @@
+//
+//  main.cpp
+//  [12100] 2048 (Easy)
+//
+//  Created by YOO TAEWOOK on 2018. 3. 30..
+//  Copyright © 2018년 YOO TAEWOOK. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void move(vector< vector<int> >&, int, bool);
+int board_size;
+int maximum = 0;
+
+int main(int argc, const char * argv[]) {
+    cin>>board_size;
+    vector< vector<int> > board(board_size, vector<int>(board_size));
+    for(int i = 0 ; i < board_size ; i++){
+        for(int j = 0 ; j < board_size ; j++){
+            cin>>board[i][j];
+        }
+    }
+    
+    move(board, 0, true);
+    
+    cout<<maximum<<endl;
+    
+    return 0;
+}
+
+void move(vector< vector<int> >& board, int depth, bool isMoved){
+    
+    if(!isMoved){ // 5번을 가지 못하고 더 이상 변경할 수 없는 경우 return
+        for(int i = 0 ; i < board_size ; i++){
+            for(int j = 0 ; j < board_size ; j++){
+                if(board[i][j] > maximum){
+                    maximum = board[i][j];
+                }
+            }
+        }
+        return;
+    }
+    
+    if(depth == 5){ // 5번을 움직였을때 리턴
+        for(int i = 0 ; i < board_size ; i++){
+            for(int j = 0 ; j < board_size ; j++){
+                if(board[i][j] > maximum){
+                    maximum = board[i][j];
+                }
+            }
+        }
+        return;
+    }
+    
+    vector< vector<int> > tempBoard; // 원본 board를 복사함
+    vector< vector<bool> > updated; // visited 개념 한 번 값이 더해진 곳은 더 이상 건들지 않는다.
+    isMoved = false; // 5번 가기전에 움직이지 못하는 상태가 있는지 체크
+    for(int dir = 0 ; dir < 4 ; dir++){
+        tempBoard = board;
+        updated.assign(board_size, vector<bool>(board_size, false));
+        if(dir == 0){ //WEST 0 2 0 2
+            for(int i = 0 ; i < board_size ; i++){ //행
+                for(int j = 1 ; j < board_size ; j++){ //열
+                    for(int k = 0 ; k < j ; k++){ //열
+                        if(tempBoard[i][j] == 0) //0은 이동 안하니까 continue
+                            continue;
+                        if(!updated[i][k]){ // 업데이트 된 적이 없다면
+                            if(tempBoard[i][k] == tempBoard[i][j]){ // 기준 인덱스에 있는 값과 비교되는 인덱스에 있는 값이 같을때
+                                bool canUpdate = true;
+                                for(int s = j - 1 ; s > k ; s--){ // 중간에 다른 숫자가 있어서 덧셈을 위한 이동이 가능한지 체크
+                                    if(tempBoard[i][s] != 0)
+                                        canUpdate = false;
+                                }
+                                if(canUpdate){ //업데이트 할 때, 변경이 있었다(isMoved = true) 업데이트 되어 더 이상 수정하지 않는다(updated[i][k] = true)를 체크
+                                    tempBoard[i][k] += tempBoard[i][j];
+                                    tempBoard[i][j] = 0;
+                                    isMoved = true;
+                                    updated[i][k] = true;
+                                }
+                            } else if(tempBoard[i][k] == 0){
+                                if(tempBoard[i][j] != 0){
+                                    tempBoard[i][k] = tempBoard[i][j];
+                                    tempBoard[i][j] = 0;
+                                    isMoved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } else if(dir == 1){ //EAST
+            for(int i = 0 ; i < board_size ; i++){ //행
+                for(int j = board_size - 2 ; j >= 0 ; j--){//열
+                    for(int k = board_size - 1 ; k > j ; k--){
+                        if(tempBoard[i][j] == 0)
+                            continue;
+                        if(!updated[i][k]){
+                            if(tempBoard[i][k] == tempBoard[i][j]){
+                                bool canUpdate = true;
+                                for(int s = j + 1 ; s < k ; s++){
+                                    if(tempBoard[i][s] != 0)
+                                        canUpdate = false;
+                                }
+                                if(canUpdate){
+                                    tempBoard[i][k] += tempBoard[i][j];
+                                    tempBoard[i][j] = 0;
+                                    isMoved = true;
+                                    updated[i][k] = true;
+                                }
+                            }else if(tempBoard[i][k] == 0){
+                                if(tempBoard[i][j] != 0){
+                                    tempBoard[i][k] = tempBoard[i][j];
+                                    tempBoard[i][j] = 0;
+                                    isMoved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } else if(dir == 2){//SOUTH
+            for(int j = 0; j < board_size ; j++){ //열
+                for(int i = board_size - 2; i >= 0; i--){ //행
+                    for(int k = board_size - 1; k > i ; k--){ //행
+                        if(tempBoard[i][j] == 0)
+                            continue;
+                        if(!updated[k][j]){
+                            if(tempBoard[k][j] == tempBoard[i][j]){
+                                bool canUpdate = true;
+                                for(int s = i + 1 ; s < k ; s++){
+                                    if(tempBoard[s][j] != 0)
+                                        canUpdate = false;
+                                }
+                                if(canUpdate){
+                                    tempBoard[k][j] += tempBoard[i][j];
+                                    tempBoard[i][j] = 0;
+                                    isMoved = true;
+                                    updated[k][j] = true;
+                                }
+                            }else if(tempBoard[k][j] == 0){
+                                if(tempBoard[i][j] != 0){
+                                    tempBoard[k][j] = tempBoard[i][j];
+                                    tempBoard[i][j] = 0;
+                                    isMoved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } else if(dir == 3){ //NORTH
+            for(int j = 0 ; j < board_size ; j++){ //열
+                for(int i = 1 ; i < board_size ; i++){ //행
+                    for(int k = 0 ; k < i ; k++){ //행
+                        if(!updated[k][j]){
+                            if(tempBoard[i][j] == 0)
+                                continue;
+                            if(tempBoard[k][j] == tempBoard[i][j]){
+                                bool canUpdate = true;
+                                for(int s = i - 1 ; s > k ; s--){
+                                    if(tempBoard[s][j] != 0)
+                                        canUpdate = false;
+                                }
+                                if(canUpdate){
+                                    tempBoard[k][j] += tempBoard[i][j];
+                                    tempBoard[i][j] = 0;
+                                    isMoved = true;
+                                    updated[k][j] = true;
+                                }
+                            }else if(tempBoard[k][j] == 0){
+                                if(tempBoard[i][j] != 0){
+                                    tempBoard[k][j] = tempBoard[i][j];
+                                    tempBoard[i][j] = 0;
+                                    isMoved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        move(tempBoard, depth + 1, isMoved);
+    }
+}
+
---
+++ [1475] 방 번호/[1475] 방 번호/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  [1475] 방 번호
+//
+//  Created by YOO TAEWOOK on 2018. 3. 30..
+//  Copyright © 2018년 YOO TAEWOOK. All rights reserved.
+//
+
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string input; //문자열로 입력 받음
+    cin>>input;
+    map<int, int> map;
+    int setNum = 0;
+    
+    
+    for(int i = 0 ; i < 10 ; i++){ //map으로 key는 각 일의 자리 숫자를, value(count)는 0을 삽입한다.
+        map.insert(pair<int, int>(i, 0));
+    }
+    
+    for(int i = 0 ; i < input.size() ; i++){
+        int num = input.at(i) - 48; // 문자열의 각 문자를 정수로 변환하기 위해 각각의 문자에서 48을 뺀다.
+        map.at(num)++;
+    }
+    
+    // 6과 9의 카운트 값을 더한다.
+    // 짝수일 경우와 홀수일 경우 필요한 숫자가 달라지므로 나눠서 처리한다.
+    int sum = map.at(6) + map.at(9);
+    if(sum % 2 == 0){
+        sum = sum / 2;
+        map.at(6) = sum;
+        map.at(9) = sum;
+    } else{
+        sum = sum / 2 + 1;
+        map.at(6) = sum;
+        map.at(9) = sum;
+    }
+    
+    for(int i = 0 ; i < map.size() - 1 ; i++){ //count 된 값 중 최대값은 필요한 세트 수를 뜻한다.
+        setNum = max(setNum, map.at(i));
+    }
+    
+    cout<<setNum<<endl;
+}
+
